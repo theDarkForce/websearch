2a878e85f6b30c706ef5c44bc0fe5ce286755cb9
theDarkForce <theDarkForce@aliyun.com>
2015-11-05 21:14:51 +0800

    Signed-off-by: theDarkForce <theDarkForce@aliyun.com>

--- searchpage.py
+++ searchpage.py
@@ -8,8 +8,60 @@ from plask import *
 import usesearch
 
 def add_title():
-    pass
+	code = "var board = document.createElement(\"div\");"
+	code += "var dc = document.createElement(\"div\");"
+	code += "for(var url in value['urllist']){"
+	code += "	"
+	code += "}"
+	code += "dc.setAttribute(\"id\",value[\"title_id\"]);"
+	code += "dc.style.margin = \"10px auto auto 0px\";"
+	code += "dc.style.width = \"1200px\";"
+	code += "dc.style.borderStyle = \"solid\";"
+	code += "dc.style.borderWidth = \"1px\";"
+	code += "dc.style.clear = \"both\";"
+	code += "var d = document.createElement(\"div\");"
+	code += "d.setAttribute(\"id\",value[\"id_1\"]);"
+	code += "d.style.fontSize = \"150%\";"
+	code += "var textnode=document.createTextNode(value[\"title\"]);"
+	code += "d.appendChild(textnode);"
+	code += "var dt = document.createElement(\"div\");"
+	code += "dt.setAttribute(\"id\",value[\"id_1_1\"]);"
+	code += "var textnode=document.createTextNode(value[\"input\"]);"
+	code += "dt.appendChild(textnode);"
+	code += "var dz = document.createElement(\"div\");"
+	code += "dz.style.fontSize = \"50%\";"
+	code += "dz.style.cssFloat=\"left\";"
+	code += "dz.style.margin = \"10px auto 5px auto\";"
+	code += "var textnode=document.createTextNode(\"作者:\");"
+	code += "dz.appendChild(textnode);"
+	code += "var dzu = document.createElement(\"div\");"
+	code += "dzu.style.cssFloat=\"left\";"
+	code += "dzu.style.fontSize = \"50%\";"
+	code += "dzu.style.margin = \"10px 5px 5px auto\";"
+	code += "var textnode=document.createTextNode(value[\"username\"]);"
+	code += "dzu.appendChild(textnode);"
+	code += "var dl = document.createElement(\"div\");"
+	code += "var textnode=document.createTextNode(\"联系方式:\");"
+	code += "dl.style.cssFloat=\"left\";"
+	code += "dl.style.fontSize = \"50%\";"
+	code += "dl.style.margin = \"10px auto 5px auto\";"
+	code += "dl.appendChild(textnode);"
+	code += "var dlc = document.createElement(\"div\");"
+	code += "var textnode=document.createTextNode(value[\"contact\"]);"
+	code += "dlc.appendChild(textnode);"
+	code += "dlc.style.fontSize = \"50%\";"
+	code += "dlc.style.margin = \"10px auto 5px auto\";"
+	code += "dc.appendChild(d);"
+	code += "dc.appendChild(dt);"
+	code += "dc.appendChild(dz);"
+	code += "dc.appendChild(dzu);"
+	code += "dc.appendChild(dl);"
+	code += "dc.appendChild(dlc);"
+	code += "if (board.firstChild){board.insertBefore(dc, board.firstChild);}else{board.appendChild(dc);}"
+	return code
 
+def on_search(p):
+    return {"urllist":usesearch.find_page(p['input'], p['index'])}
 
 def layout():
     app = plaskapp('0.0.0.0', 5000)
@@ -20,21 +72,24 @@ def layout():
     c = pycontainer('title_input', pyhtmlstyle.margin_auto, p)
     c.set_location(580, 350)
 
+    pnotes = pytext("千度又如何", "notes", pyhtmlstyle.margin_auto, c)
+    pnotes.set_font_size(200)
+    pnotes.set_font_color((100, 100, 200))
+    pnotes.left = 100
     titletitle = pyedit('title_edit', pyedit.text, pyhtmlstyle.float_left, c)
     titletitle.set_size(300, 20)
     titletitle.set_location(10, 0)
-    button = pybutton('千度呢', 'button', pyhtmlstyle.float_left, c)
-    button.set_size(60, 26)
+    button = pybutton('千度吧', 'button', pyhtmlstyle.float_left, c)
+    button.set_size(80, 20)
     button.set_location(3, 0)
     ev = uievent('http://127.0.0.1:5000/', button, pyelement.onclick)
-    ev.add_call_ui(add_title)
     params = jparams()
     params.append("input", titletitle.client_get_input_text())
+    params.append("index", '0')
     onsev = on_server_response()
     sev = server_event("submit", params, onsev)
-    def on_click(p):
-        return usesearch.find_page(p['input'])
-    sev.add_onevent(on_click)
+    sev.add_onevent(on_search)
+    onsev.add_call(c.remove())
     onsev.add_call(add_title())
     ev.add_server_event(sev)
     button.register_uievent(ev)
--- usesearch.py
+++ usesearch.py
@@ -4,28 +4,55 @@
 # autor: qianqians
 import sys
 sys.path.append('../')
+from pagecache import *
 import pymongo
+import time
 from doclex import doclex
 
 collection_page = None
 collection_key = None
 
-def find_page(input):
+def find_page(input, index):
     keys = doclex.splityspace(input)
     for k in keys:
         collection_key.insert({"key":k})
+
+    if cache_page.has_key(input):
+        cache_page['input']['timetmp'] = time.time()
+        return cache_page['input']['urllist']
+
     page_list = []
     for k in keys:
-        c = collection_page.find({"key":k})
-        for i in c:
-            page_list.append(i)
+        if key_page.has_key(k):
+            key_page['input']['timetmp'] = time.time()
+            page_list = key_page['input']['urllist']
+        else:
+            c = collection_page.find({"key":k})
+            for i in c:
+                page_list.append(i)
+            key_page['input'] = {}
+            key_page['input']['timetmp'] = time.time()
+            key_page['input']['urllist'] = page_list
+
     if len(page_list) == 0:
         keys = doclex.lex(keys)
         for k in keys:
-            c= collection_page.find({"key":k})
-        for i in c:
-            page_list.append(i)
-    return page_list
+            if key_page.has_key(k):
+                key_page['input']['timetmp'] = time.time()
+                page_list = key_page['input']['urllist']
+            else:
+                c = collection_page.find({"key":k})
+                for i in c:
+                    page_list.append(i)
+                key_page['input'] = {}
+                key_page['input']['timetmp'] = time.time()
+                key_page['input']['urllist'] = page_list
+
+    cache_page['input'] = {}
+    cache_page['input']['timetmp'] = time.time()
+    cache_page['input']['urllist'] = page_list
+
+    return page_list[index*10: index*1+10]
 
 def init():
     conn = pymongo.Connection('localhost',27017)
